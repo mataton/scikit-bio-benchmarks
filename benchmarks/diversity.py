822c00e41bd581c1adc4e41d29ee4e32712c866d
Matt Aton <matthewaton2@gmail.com>
2025-06-11 13:29:14 -0700

    Initial draft, NOT FUNCTIONAL

---
+++ benchmarks/diversity.py
@@ -0,0 +1,28 @@
+"""Basic benchmarks on skbio.stats."""
+
+from skbio.diversity.alpha import faith_pd
+from skbio.tree import TreeNode
+import numpy as np
+from io import StringIO
+
+class SequenceSuite:
+
+    def setup(self):
+        self.counts = np.array([0, 1, 1, 4, 2, 5, 2, 4, 1, 2])
+        self.b1 = np.array([[1, 3, 0, 1, 0],
+                            [0, 2, 0, 4, 4],
+                            [0, 0, 6, 2, 1],
+                            [0, 0, 1, 1, 1],
+                            [2, 0, 3, 0, 0]])
+        self.sids1 = list('ABCDE')
+        self.oids1 = ['OTU%d' % i for i in range(1, 6)]
+        self.t1 = TreeNode.read(StringIO(
+            '(((((OTU1:0.5,OTU2:0.5):0.5,OTU3:1.0):1.0):'
+            '0.0,(OTU4:0.75,OTU5:0.75):1.25):0.0)root;'))
+        self.t1_w_extra_tips = TreeNode.read(
+           StringIO('(((((OTU1:0.5,OTU2:0.5):0.5,OTU3:1.0):1.0):0.0,(OTU4:'
+                    '0.75,(OTU5:0.25,(OTU6:0.5,OTU7:0.5):0.5):0.5):1.25):0.0'
+                    ')root;'))
+
+    def time_faith_pd(self):
+        return faith_pd(self.b1[0], self.oids1, self.t1)
